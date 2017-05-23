a726668746db34afe72b0d3522d3083c0a311463
kyuwankim <lpnids@gmail.com>
2017-05-23 12:08:04 +0900

    Add Spinner

--- app/src/main/java/com/kyuwankim/android/adapterbasic/MainActivity.java
+++ app/src/main/java/com/kyuwankim/android/adapterbasic/MainActivity.java
@@ -1,13 +1,45 @@
 package com.kyuwankim.android.adapterbasic;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    String datas[] = {"선택", "ListView", "GridView", "RecyclerView"};
+
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinner = (Spinner) findViewById(R.id.spinner);
+        // List뷰에 데이터를 연결하기위한 순서
+        // 1. 데이터 정의
+        // datas로 정의함
+        // 2. 아답터 생성
+        // ArrayAdapter 생성
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, datas);
+
+        // 3. 뷰에 아답터 연결
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, datas[position], Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
     }
 }
